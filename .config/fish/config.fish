b70322f50e0bcc35df955eaa24fd5c25dacb12dd
Roman Pisarew <roman.pisarew@gmail.com>
2025-09-21 23:22:22 +0200

    chore: autostart zellij

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,19 +11,24 @@ set -gx ZELLIJ_AUTO_ATTACH true
 
 # Commands to run in interactive sessions can go here
 if status is-interactive
-  fish_default_key_bindings
+    fish_default_key_bindings
 
-  # Fast, minimal greeting
-  set -U fish_greeting
+    # Fast, minimal greeting
+    set -U fish_greeting
 
-  # gpg
-  # https://superuser.com/questions/1628782/gpg-signing-failed-no-pinentry
-  set -x GPG_TTY (tty)
-  # gpg-agent --daemon > /dev/null 2>&1
+    # gpg
+    # https://superuser.com/questions/1628782/gpg-signing-failed-no-pinentry
+    set -x GPG_TTY (tty)
+    # gpg-agent --daemon > /dev/null 2>&1
 
-  function __atuin_lazy_init --on-event fish_prompt
-    functions -e __atuin_lazy_init
-    source (atuin init fish | psub)
-  end
+    function __atuin_lazy_init --on-event fish_prompt
+        functions -e __atuin_lazy_init
+        source (atuin init fish | psub)
+    end
+
+    # autostart zellij
+    if set -q ZELLIJ
+    else
+        zellij
+    end
 end
-
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,7 +2,8 @@ local opt = vim.opt
 
 vim.g.autoformat = false
 
-opt.colorcolumn = "80,100"
+opt.signcolumn = "no"
+opt.colorcolumn = "91,111" -- 11 cols offset because of LazyVim statuscolumn
 opt.fileencoding = "utf-8"
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.wrap = true
