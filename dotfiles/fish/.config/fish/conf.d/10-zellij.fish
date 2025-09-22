9ccdf336e712f649536f2856cb6d15dab167d49b
Roman Pisarew <roman.pisarew@gmail.com>
2025-09-22 23:17:21 +0200

    chore: update

--- dotfiles/fish/.config/fish/conf.d/10-zellij.fish
+++ dotfiles/fish/.config/fish/conf.d/10-zellij.fish
@@ -4,7 +4,7 @@ status is-interactive; or exit
 # --- Autostart Zellij ----------------------------------------------------
 set -gx ZELLIJ_AUTO_ATTACH true
 
-if set -q ZELLIJ
-else
-    zellij
-end
+# if set -q ZELLIJ
+# else
+#     zellij
+# end
