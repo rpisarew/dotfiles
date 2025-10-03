ce5cae98188b0703cab6ca2d4a9413004fe29dfa
Foo Bar <foo@bar.com>
2025-10-03 02:50:05 +0200

    chore: update

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,26 +3,37 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- frontend: "OpenGL", "WebGpu", or "Software"
-config.front_end = "WebGpu"
+-- config.front_end = "OpenGL"
+config.enable_wayland = true
+-- config.prefer_egl = true
+config.max_fps = 140
 
-config.font = wezterm.font("FiraCode Nerd Font", {
-  weight = "Medium",
-  stretch = "Normal",
-  style = "Normal",
+-- config.font = wezterm.font("FiraCode Nerd Font", {
+--   weight = "Medium",
+--   stretch = "Normal",
+--   style = "Normal",
+-- })
+config.font = wezterm.font_with_fallback({
+  { family = "FiraCode Nerd Font", weight = "Medium" }
 })
-config.font_size = 16
+
+config.font_size = 12
 
 config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 -- config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.85
+config.window_background_opacity = 0.75
 -- config.macos_window_background_blur = 10
 
+config.colors = {
+  background = 'black'
+}
+
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
 }
 
 return config
